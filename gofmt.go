01052d09a0cc0989792e4048d9811482e3f28a18
yolksys <cyf.signup@gmail.com>
2025-04-14 07:39:26 +0000

    Update gofmt.go

--- gofmt.go
+++ gofmt.go
@@ -61,8 +61,8 @@ var version = ""
 
 // Keep these in sync with go/format/format.go.
 const (
-	tabWidth    = 8
-	printerMode = printer.UseSpaces | printer.TabIndent | printerNormalizeNumbers
+	tabWidth    = 2
+	printerMode = printer.UseSpaces
 
 	// printerNormalizeNumbers means to canonicalize number literal prefixes
 	// and exponents while printing. See https://golang.org/doc/go1.13#gofmt.
